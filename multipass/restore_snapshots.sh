15e84f70857c98fc6e7b95d33ff0c908c14803f1
Vasile <baluta.vasile@gmail.com>
2024-08-12 12:31:37 +0200

    add missing commands
    to make it a full working script

--- multipass/restore_snapshots.sh
+++ multipass/restore_snapshots.sh
@@ -7,5 +7,8 @@ instances=("kubemaster" "${workers[@]}")
 # Restore snapshots
 for instance in "${instances[@]}"; do
   echo "Restoring snapshot of $instance..."
-  multipass restore "$instance".snapshotx
+  multipass stop "$instance"
+  echo "If the restore fails be shure you specified the correct snapshot version: e.g $instance.snapshot1 for version 1"
+  multipass restore --destructive "$instance".snapshot
+  multipass start "$instance"
 done
